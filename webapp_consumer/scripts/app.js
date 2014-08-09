819291caf107184c1e4c20aca7dbe5ccd1358688
hiqbal01 <haris.iqbal91@gmail.com>
2014-08-09 19:02:50 -0400

    CHanged Image. Added more to the Summary Page.

--- webapp_consumer/scripts/app.js
+++ webapp_consumer/scripts/app.js
@@ -7,8 +7,10 @@ var App = (function (App, $) {
     /**
      * App.init()
      */
+	App.payout = 200
     App.init = function () {
 		App.setMenu()
+		App.displayPayout()
     }
 	App.setMenu = function(){
 		$(".user_icon, .user_menu").mouseenter(function(e){
@@ -19,5 +21,14 @@ var App = (function (App, $) {
 			$(".user_menu").hide()
 		})
 	}
+	App.displayPayout = function(){
+		if((App.payout+"").indexOf(".")<0){
+			$(".hk_payoutValue").html("$" + App.payout)
+			$(".hk_payoutDecimal").html(".00")
+		}else{
+			$(".hk_payoutValue").html("$" + App.payout.split(".")[0])
+			$(".hk_payoutDecimal").html(App.payout.split(".")[1] + ".00")
+		}
+	}		
     return App;
 }(App || {}, jQuery));
