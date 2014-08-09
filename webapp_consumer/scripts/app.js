4ba18af50f88edbbd3e163ed745ade54ee1e2d09
hiqbal01 <haris.iqbal91@gmail.com>
2014-08-09 16:47:43 -0400

    Added to consumer app

--- webapp_consumer/scripts/app.js
+++ webapp_consumer/scripts/app.js
@@ -8,8 +8,16 @@ var App = (function (App, $) {
      * App.init()
      */
     App.init = function () {
-		
+		App.setMenu()
     }
-
+	App.setMenu = function(){
+		$(".user_icon, .user_menu").mouseenter(function(e){
+			e.preventDefault()
+			$(".user_menu").show()
+		}).mouseleave(function(e){
+			e.preventDefault()
+			$(".user_menu").hide()
+		})
+	}
     return App;
 }(App || {}, jQuery));
