df59c61593aa8524b24c2b79c0e68c8a8bb6b639
Haris Iqbal <harrisiqba@gmail.com>
2014-08-10 03:24:42 -0400

    Added Have Know Page

--- webapp_consumer/scripts/app.js
+++ webapp_consumer/scripts/app.js
@@ -27,11 +27,27 @@ var App = (function (App, $) {
 		"phone": "false",
 		"email":"true"
 	}]
+    App.haveKnowObject = [{
+        "q":"What street were you living on in Dec 2010?",
+        "a":"Grimauld Place"
+    },
+    {
+        "q":"Which of the following pitcures shows your brother when he was a kid?",
+        "a":"images/brother.png"
+    },
+    {
+        "q":"What is your favorite uncle's name?",
+        "a":"Uncle Mats"
+    },
+    {
+        "q":"Who is your favorite fictional Character",
+        "a":"Tyrion Lannister"
+    }]
     App.init = function () {
 		App.setMenu()
 		App.displayPayout()
 		App.setButtons()
-		App.displayModes($("contact_name")[0].html())
+		App.displayModes($(".contact_name").eq(0).html())
     }
 	App.setMenu = function(){
 		$(".user_icon, .user_menu").mouseenter(function(e){
@@ -74,9 +90,15 @@ var App = (function (App, $) {
 		$(".all_body").hide()
 		$(".summary_body").fadeIn()
 		App.displayPayout()
-		App.displayModes($("contact_name")[0].html())
-		$(".menu_option.hidden_option").removeClass("hidden_option")
+		setTimeout(function(){App.displayModes($("contact_name").eq(0).html())},300)
+		$(".hidden_option").removeClass("hidden_option")
 		$(".summary_option").addClass("hidden_option")
+        $(".contact").off("click.getModes").on("click.getModes", function(e){
+            e.preventDefault()
+            $(this).addClass("selected")
+            $(this).siblings().removeClass("selected")
+            App.displayModes($(this).find(".contact_name").html())
+        })
 	}
 	App.setButtons = function(){
 		$(".take_to_payload, .wallet_option").off("click.take_to_payload").on("click.take_to_payload", function(e){
@@ -101,15 +123,45 @@ var App = (function (App, $) {
                }
             lastScrollTop = st;	
 		})
+        $(".contact").off("click.getModes").on("click.getModes", function(e){
+            e.preventDefault()
+            $(this).addClass("selected")
+            $(this).siblings().removeClass("selected")
+            App.displayModes($(this).find(".contact_name").html())
+        })
+        $(".haveknows_option").off("click.getHaveKnow").on("click.getHaveKnow", function(e){
+            e.preventDefault()
+            App.setHaveKnow()
+        })
 	}
 	App.displayModes = function(name){
 		var relJSON = App.contactObj.filter(function(a){return a.user==name})[0]
-		if(relJSON.phone == "true") $(".phoneMode").addClass("enabled")
-		else $(".phoneMode").removeClass("enabled")
-		if(relJSON.text == "true") $(".textMode").addClass("enabled")
-		else $(".textMode").removeClass("enabled")
-		if(relJSON.email == "true") $(".emailMode").addClass("enabled")
-		else $(".emailMode").removeClass("enabled")
+        console.log(relJSON)
+		if(relJSON.phone == "true") $(".phoneMode").addClass("enabled").removeClass("disabled")
+		else $(".phoneMode").removeClass("enabled").addClass("disabled")
+		if(relJSON.text == "true") $(".textMode").addClass("enabled").removeClass("disabled")
+		else $(".textMode").removeClass("enabled").addClass("disabled")
+		if(relJSON.email == "true") $(".emailMode").addClass("enabled").removeClass("disabled")
+		else $(".emailMode").removeClass("enabled").addClass("disabled")
 	}
+    App.setHaveKnow = function(){
+        $(".all_body").hide()
+        $(".haveKnow_body").fadeIn()
+        $(".menu_option.hidden_option").removeClass("hidden_option")
+		$(".haveknows_option").addClass("hidden_option")
+        var html = ""
+        $.each(App.haveKnowObject, function(i){
+            html = '<div class="know"><span class="knowIcon fa fa-question"></span><span class="knowText">'+App.haveKnowObject[i].q+'</span><span class="knowPoints">+5</span></div>'
+            $(".knowCont").append(html)
+            if($(".knowText").eq(i).height()<$(".know").height()) $(".knowText").eq(i).css("top", ($(".know").height()-$(".knowText").eq(i).height())/2)
+        })
+        $(window).resize(function(){
+            $.each(App.haveKnowObject, function(i){
+                $(".knowText").eq(i).css("top", 0)
+                if($(".knowText").eq(i).height()<$(".know").height()) $(".knowText").eq(i).css("top", ($(".know").height()-$(".knowText").eq(i).height())/2)
+            })
+        })
+        
+    }
     return App;
 }(App || {}, jQuery));
